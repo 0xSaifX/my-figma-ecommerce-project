8627a3b658a4bad871cabd5a9b2df60027734918
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-08 14:51:23 +0100

    auto-commit

--- src/pages/HomeFashion.jsx
+++ src/pages/HomeFashion.jsx
@@ -225,7 +225,7 @@ const dayOfDealProducts = [
             </div>
           </div>
 
-          <div className="main-section">
+          <div className="">
 
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
   {categoryIcons.map(cat => (
