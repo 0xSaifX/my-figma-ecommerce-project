862cf4ecf291883cc407ccc3735cd10e9d4f1efd
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-08 20:16:06 +0100

    auto-commit

--- src/pages/HomeFashion.jsx
+++ src/pages/HomeFashion.jsx
@@ -462,9 +462,8 @@ const dayOfDealProducts = [
   <div
     className="
       grid 
-      grid-cols-1 
-      sm:grid-cols-2 
-      md:grid-cols-3 
+      grid-cols-2 
+      sm:grid-cols-3 
       lg:grid-cols-4 
       gap-5
     "
