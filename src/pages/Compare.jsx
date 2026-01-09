99661df0357d22523a6e0ccb1dbe21e4873d6b27
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-09 10:31:36 +0100

    auto-commit

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -50,137 +50,154 @@ import { FaStar } from "react-icons/fa";
   const star = { color: "#f6a700", marginRight: "2px" };
 
 
-function Compare () {
-  return(
-    <div>
+function Compare() {
+  return (
+    <div className="px-4 md:px-8 lg:px-32 my-16">
+      <div className="overflow-x-auto">
+        <table className="min-w-[900px] w-full border border-gray-200 text-sm">
+          <tbody>
+            {/* Product Images */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-4 sticky left-0 z-10">
+                Product Image
+              </td>
 
-      <div style={{ margin: "4rem 8rem" }}>
+              {[perfume, shirt, berry].map((img, i) => (
+                <td key={i} className="relative text-center p-6 border">
+                  <button className="absolute top-2 right-2 text-gray-400 hover:text-red-500">
+                    ✕
+                  </button>
+                  <img
+                    src={img}
+                    alt="product"
+                    className="mx-auto max-h-36 object-contain"
+                  />
+                </td>
+              ))}
+            </tr>
 
-      <table style={tableStyle}>
-        <tbody>
-          <tr>
-            <td style={leftHeaderStyle}>Product Image</td>
+            {/* Name */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Name
+              </td>
+              <td className="p-4 border">Long lasting perfume</td>
+              <td className="p-4 border">Men's stylish printed shirt</td>
+              <td className="p-4 border">Blue berry</td>
+            </tr>
 
-            <td style={{ ...cellStyle, position: "relative" }}>
-              <span style={removeIcon}>✕</span>
-              <div style={productImgContainer}>
-                <img
-                  src={perfume}
-                  style={productImg}/>
-              </div>
-            </td>
+            {/* Category */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Category
+              </td>
+              <td className="p-4 border">Perfume</td>
+              <td className="p-4 border">Men's Wear</td>
+              <td className="p-4 border">Fresh Fruits</td>
+            </tr>
 
-            <td style={{ ...cellStyle, position: "relative" }}>
-              <span style={removeIcon}>✕</span>
-              <div style={productImgContainer}>
-                <img
-                  src={shirt}
-                  style={productImg}/>
-              </div>
-            </td>
+            {/* Ratings */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Ratings
+              </td>
 
-            <td style={{ ...cellStyle, position: "relative" }}>
-              <span style={removeIcon}>✕</span>
-              <div style={productImgContainer}>
-                <img
-                  src= {berry}
-                  style={{ maxWidth: "180px" }}/>
-              </div>
-            </td>
-          </tr>
+              {[1, 2, 3].map((_, i) => (
+                <td key={i} className="p-4 border">
+                  <div className="flex items-center gap-2 text-yellow-500">
+                    {[...Array(5)].map((_, j) => (
+                      <FaStar key={j} />
+                    ))}
+                    <span className="text-gray-500">(654 Reviews)</span>
+                  </div>
+                </td>
+              ))}
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Name</td>
-            <td style={cellStyle}>Long lasting perfume</td>
-            <td style={cellStyle}>Men's stylish printed shirt</td>
-            <td style={cellStyle}>Blue berry</td>
-          </tr>
+            {/* Availability */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Availability
+              </td>
+              <td className="p-4 border text-red-500 font-medium">
+                Out Of Stock
+              </td>
+              <td className="p-4 border text-green-600 font-medium">
+                Available
+              </td>
+              <td className="p-4 border text-red-500 font-medium">
+                Out Of Stock
+              </td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Category</td>
-            <td style={cellStyle}>perfume</td>
-            <td style={cellStyle}>men's wear</td>
-            <td style={cellStyle}>Fresh Fruits</td>
-          </tr>
+            {/* Location */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Location
+              </td>
+              <td className="p-4 border">In Store, Online</td>
+              <td className="p-4 border">Online</td>
+              <td className="p-4 border">Online</td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Ratings</td>
-            <td style={cellStyle}>
-              <span style={star}><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></span> (654 Review)
-            </td>
-            <td style={cellStyle}>
-              <span style={star}><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></span> (654 Review)
-            </td>
-            <td style={cellStyle}>
-              <span style={star}><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></span> (654 Review)
-            </td>
-          </tr>
+            {/* Brand */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Brand
+              </td>
+              <td className="p-4 border">Bhisma Organice</td>
+              <td className="p-4 border">Bhisma Organice</td>
+              <td className="p-4 border">Bhisma Organice</td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Availability</td>
-            <td style={{ ...cellStyle, ...redText }}>Out Of Stock</td>
-            <td style={{ ...cellStyle, ...greenText }}>Available</td>
-            <td style={{ ...cellStyle, ...redText }}>Out Of Stock</td>
-          </tr>
+            {/* SKU */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                SKU
+              </td>
+              <td className="p-4 border">55555</td>
+              <td className="p-4 border">24433</td>
+              <td className="p-4 border">23456</td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Location</td>
-            <td style={cellStyle}>in Store, Online</td>
-            <td style={cellStyle}>Online</td>
-            <td style={cellStyle}>Online</td>
-          </tr>
+            {/* Quantity */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Quantity
+              </td>
+              <td className="p-4 border">1 Pack</td>
+              <td className="p-4 border">1 Pack</td>
+              <td className="p-4 border">1 Pack</td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>Brand</td>
-            <td style={cellStyle}>Bhisma Organice</td>
-            <td style={cellStyle}>Bhisma Organice</td>
-            <td style={cellStyle}>Bhisma Organice</td>
-          </tr>
+            {/* Weight */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Weight
+              </td>
+              <td className="p-4 border">5 pcs</td>
+              <td className="p-4 border">1 pcs</td>
+              <td className="p-4 border">500 g</td>
+            </tr>
 
-          <tr>
-            <td style={leftHeaderStyle}>SKU</td>
-            <td style={cellStyle}>55555</td>
-            <td style={cellStyle}>24433</td>
-            <td style={cellStyle}>23456</td>
-          </tr>
-
-          <tr>
-            <td style={leftHeaderStyle}>Quantity</td>
-            <td style={cellStyle}>1 Pack</td>
-            <td style={cellStyle}>1 Pack</td>
-            <td style={cellStyle}>1 Pack</td>
-          </tr>
-
-          <tr>
-            <td style={leftHeaderStyle}>Weight</td>
-            <td style={cellStyle}>5 pcs</td>
-            <td style={cellStyle}>1 pcs</td>
-            <td style={cellStyle}>500 g</td>
-          </tr>
-
-          <tr>
-            <td style={leftHeaderStyle}>Description</td>
-            <td style={cellStyle}>
-              Recently with desktop publishing software like Aldus PageMaker
-              including versions of Lorem Ipsum.
-            </td>
-            <td style={cellStyle}>
-              Recently with desktop publishing software like Aldus PageMaker
-              including versions of Lorem Ipsum.
-            </td>
-            <td style={cellStyle}>
-              Recently with desktop publishing software like Aldus PageMaker
-              including versions of Lorem Ipsum.
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            {/* Description */}
+            <tr>
+              <td className="bg-gray-100 font-semibold px-4 py-3 sticky left-0 z-10">
+                Description
+              </td>
+              {[1, 2, 3].map((_, i) => (
+                <td key={i} className="p-4 border text-gray-600">
+                  Recently with desktop publishing software like Aldus PageMaker
+                  including versions of Lorem Ipsum.
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-
-    
-
-  </div>
-  )
+  );
 }
+
 
 export default Compare;
