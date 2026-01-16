cf924d41ca9ae26245342bde10941d0abc274f1f
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-16 09:51:50 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     minify: 'esbuild', // LightningCSS sometimes fails with broken CSS
+    outDir: 'dist',
   },
-  resolve:{
-  alias:{
-    '@': path.resolve(__dirname,'./src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  },
-  server:{
-    port:5173,
+  server: {
+    port: 5173,
   },
 })
 
